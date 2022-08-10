4214b8c07e480044864842e2de64eea800dacdc7
Henri Schulte <hens@itu.dk>
2022-08-10 20:46:32 +0200

    Added wifi indicator, paging mechanism

--- NodeMCUWeatherStation/NodeMCUWeatherStation.ino
+++ NodeMCUWeatherStation/NodeMCUWeatherStation.ino
@@ -13,8 +13,24 @@ String urlPath = "";
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(D0, D1, D5, D6, D7, D8);
 
+const int ledPin = D2;
+const int buttonPin = D3;
+
+int page = 0;
+const int maxPage = 1; // index for the last page
+bool nextPageOnUp = false; // flag for switching to next page when button is released
+
+double currentTemp;
+
 void setup() {
   Serial.begin(9600); 
+
+  // set pin modes
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+
+  // LED starts off
+  digitalWrite(ledPin, LOW);
 
   // set up the LCD's number of columns and rows
   lcd.begin(16, 2);
@@ -23,62 +39,106 @@ void setup() {
   Serial.println("Connecting");
   lcd.print("Connecting");
   while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
+    // blink
+    digitalWrite(ledPin, HIGH);
+    delay(250);
+    digitalWrite(ledPin, LOW);
+    delay(250);
     Serial.print(".");
   }
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
-  lcd.setCursor(0, 0);
-  lcd.print("Connected   ");
+  digitalWrite(ledPin, HIGH); // turn LED on
+
+  displayCurrentTemp();
 }
 
 void loop() {
-  //Check WiFi connection status
-  if(WiFi.status()== WL_CONNECTED){
-    WiFiClient client;
-    HTTPClient http;
-    
-    // Your Domain name with URL path or IP address with path
-    http.begin(client, urlPath);
-    
-    // Send HTTP GET request
-    int httpResponseCode = http.GET();
-    
-    if (httpResponseCode>0) {
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
-      String response = http.getString();
-      //Serial.println(response);
-
-      // Decode JSON response
-      JSONVar responseJSON = JSON.parse(response);
-      if (JSON.typeof(responseJSON) == "undefined") {
-        Serial.println("Parsing input failed!");
-      } else {
-        double temp = responseJSON["features"][0]["properties"]["value"];
-        Serial.print("Temperature: ");
-        Serial.print(temp);
-        Serial.print("°C");
-        Serial.println();
-
-        lcd.setCursor(0, 1);
-        lcd.print("Temp: ");
-        lcd.print(temp);
-        lcd.print(" C ");
+  if (digitalRead(buttonPin) == LOW) { // button is pressed
+    if (nextPageOnUp == false) {
+      nextPageOnUp = true; // switch to next page when button is released
+    }
+  } else { 
+    if (nextPageOnUp == true) { // button is released
+      nextPageOnUp = false; // ignore button state until it is pressed again
+      page ++;
+      page = page % (maxPage + 1); // increment page index but roll over if exceeding the last page
+      lcd.clear();
+      switch(page) {
+        case 0:
+          displayCurrentTemp();
+          break;
+        case 1:
+          displayForecast();
+          break;
       }
     }
-    else {
-      Serial.print("Error code: ");
-      Serial.println(httpResponseCode);
+  }
+
+  
+}
+
+void displayCurrentTemp() {
+  if (!currentTemp) {
+    currentTemp = getCurrentTemp();
+  }
+  // Display temperature
+  lcd.setCursor(0, 0);
+  lcd.print("Current temp: ");
+  lcd.setCursor(0, 1);
+  lcd.print(currentTemp);
+  lcd.print(" C ");
+  
+}
+
+double getCurrentTemp() {
+  WiFiClient client;
+  HTTPClient http;
+  
+  // Your Domain name with URL path or IP address with path
+  http.begin(client, urlPath);
+  
+  // Send HTTP GET request
+  int httpResponseCode = http.GET();
+  
+  if (httpResponseCode > 0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    String response = http.getString();
+    //Serial.println(response);
+    double temp;
+
+    // Decode JSON response
+    JSONVar responseJSON = JSON.parse(response);
+    if (JSON.typeof(responseJSON) == "undefined") {
+      Serial.println("Parsing input failed!");
+    } else {
+      temp = responseJSON["features"][0]["properties"]["value"];
+      Serial.print("Temperature: ");
+      Serial.print(temp);
+      Serial.print("°C");
+      Serial.println();
     }
-    // Free resources
-    http.end();
+
+    // blink after update
+    digitalWrite(ledPin, LOW);
+    delay(250);
+    digitalWrite(ledPin, HIGH);
+
+    return temp;
   }
   else {
-    Serial.println("WiFi Disconnected");
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
   }
-  
-  // Delay until next check
-  delay(300000);
+  // Free resources
+  http.end();
+}
+
+void displayForecast() {
+  lcd.setCursor(0, 0);
+  lcd.print("Forecast:");
+  lcd.setCursor(0, 1);
+  lcd.print("Great weather");
 }
